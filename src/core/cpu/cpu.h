ff6b4d5c0695a23c214883af664eca4cc2691c65
Davide Verlezza <davideverlezza20@gmail.com>
2026-09-12 20:08:13 +0200

    feat(cpu): implement or a,r8

--- src/core/cpu/cpu.h
+++ src/core/cpu/cpu.h
@@ -131,6 +131,8 @@ private:
                                             uint8_t and_a_hl();
     template <uint8_t Cpu::Registers::*Reg> uint8_t xor_a_r8();
                                             uint8_t xor_a_hl();
+    template <uint8_t Cpu::Registers::*Reg> uint8_t or_a_r8();
+                                            uint8_t or_a_hl();
 };
 
 
