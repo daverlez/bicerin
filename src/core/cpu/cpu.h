2cc3b1e684c005de11a9e89190f507567f37f5f2
Davide Verlezza <davideverlezza20@gmail.com>
2026-09-12 18:51:17 +0200

    feat(cpu): implement and a,r8

--- src/core/cpu/cpu.h
+++ src/core/cpu/cpu.h
@@ -127,6 +127,8 @@ private:
                                             uint8_t sub_a_hl();
     template <uint8_t Cpu::Registers::*Reg> uint8_t sbc_a_r8();
                                             uint8_t sbc_a_hl();
+    template <uint8_t Cpu::Registers::*Reg> uint8_t and_a_r8();
+                                            uint8_t and_a_hl();
 };
 
 
